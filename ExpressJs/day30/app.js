f1e3bc30a61aa9aadaf843ad543e38e6d73132d8
faizaRami <rami.faiza06@gmail.com>
2023-09-27 15:01:25 +0100

    change file

--- ExpressJs/day30/app.js
+++ ExpressJs/day30/app.js
@@ -15,7 +15,7 @@ let products = [
     { id: 2, description:'black Samsung Galaxy S21 2021', name: 'Samsung Galaxy S21', price: 999.99, image:'2.jpg' },
     { id: 3, description:'white Sony PlayStation 5 2022', name: 'Sony PlayStation 5', price: 499.99, image:'3.jpg' },
     { id: 4, description:'black MacBook Pro 16 2025', name: 'MacBook Pro 16', price: 2399.99, image:'4.jpg' },
-    { id: 5, description:'grey DJI Mavic Air 2 2002', name: 'DJI Mavic Air 2', price: 799.99, image:'5.jpg' },
+    { id: 5, description:'grey DJI Mavic Air 2 2002', name: 'DJI Mavic Air 3', price: 799.99, image:'5.jpg' },
 ];
 
 app.get('/', (req, res) => {
